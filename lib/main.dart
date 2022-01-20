14aabe0c519a954f92f4a751758b8da3e90e2466
Vishesh Kumar <vk7879@srmist.edu.in>
2022-01-20 22:48:21 +0530

    1.03

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
@@ -16,20 +17,24 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    var  leftDicenumber = 3;
     return Center(
       child: Row(
         children: [
           Expanded(
-            child: FlatButton(onPressed: (){
-              print('Left Button Got Pressed');
+            child: MaterialButton(onPressed: (){
+              print('Left Button Got Pressed.');
             },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDicenumber.png'),//String Interpolation
             ),
           ),
           Expanded(
-            child: FlatButton(
+            child: MaterialButton( onPressed: (){
+              print('Right Button Got Presses.');
+            },
               child: Image.asset('images/dice2.png'),
             ),
           )
